ec5504950f9a03dd2919b26ecfda9acd675faf5b
adarsh-technocrat <adarshkumarsingh865@gmail.com>
2023-01-16 11:41:23 +0530

    chore: added minor change to Spotify section

--- src/Components/CommonComponent/Footer/SpotifySection.jsx
+++ src/Components/CommonComponent/Footer/SpotifySection.jsx
@@ -10,17 +10,22 @@ function SpotifySection() {
     });
   }, []);
 
+  function getValue() {
+    if (data != null) {
+      if (data?.item != null) {
+        return data.item.name;
+      } else {
+        return "Playing Ad 😅";
+      }
+    }
+    return "Not Playing";
+  }
+
   return (
     <div class="pt-3 pb-11 flex flex-row items-center ">
       <img class="pr-6" src="/assets/sportify_logo.svg" alt="" />
-      <text class="font-semibold text-dark-text-white text-2xl pr-6">
-        {data != null
-          ? data.currently_playing_type !== "ad"
-            ? data.item.name
-            : "Playing Ad 😅"
-          : "Not Playing"}
-      </text>
-      <text class="font-semibold text-dark-text-grey text-2xl ">–Spotify</text>
+      <text class="font-semibold text-dark-text-white text-2xl pr-6">{getValue()}</text>
+      <text class="font-semibold text-dark-text-grey text-2xl ">– Spotify</text>
     </div>
   );
 }
