d08753bc840ad1f5044e13670a0e53f08dd51e2f
adarsh-technocrat <adarshkumarsingh865@gmail.com>
2023-11-02 11:26:29 +0530

    chore: added Cards in creator mode section

--- src/pages/Blog/Components/Community/Community.jsx
+++ src/pages/Blog/Components/Community/Community.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Community() {
   return (
-    <section className="pl-32 pr-32 pb-44">
+    <section className="pl-32 pr-32">
       <h1 className="text-5xl font-bold text-[#ffffff]">Community</h1>
       <div className="mt-20 h-[430px] w-auto bg-[#D9D9D9] rounded-sm"></div>
     </section>
--- src/pages/Blog/Components/CreatorModeOn/CreatorModeOn.jsx
+++ src/pages/Blog/Components/CreatorModeOn/CreatorModeOn.jsx
@@ -11,6 +11,19 @@ function CreatorModeOn() {
         <br />
         This course is 12 hours long and is completely live streamed.
       </p>
+
+      <div className="div">
+        <div className="flex justify-start items-start mt-10 gap-3">
+          {Array.from({ length: 4 }).map((item) => (
+            <div className="bg-[#D9D9D9] rounded-md w-[86px] h-[36px] opacity-50" key={item}></div>
+          ))}
+        </div>
+        <div className="flex justify-start items-start mt-10 gap-3">
+          {Array.from({ length: 4 }).map((item) => (
+            <div className="bg-[#D9D9D9] rounded-md w-full h-[600px]" key={item}></div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
